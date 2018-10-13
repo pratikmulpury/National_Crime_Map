c2fe202c2ba075410ec9d49100002412c908259c
Pratik Mulpury <pratik@Pratiks-MacBook-Pro.local>
2018-10-13 15:57:41 -0400

    second comitt

--- server/src/app.js
+++ server/src/app.js
@@ -7,7 +7,6 @@ const morgan = require('morgan')
 
 // set up google oauth2client
 const {OAuth2Client} = require('google-auth-library');
-const client = new OAuth2Client('677447604640-rnbtov97fi21skrh09u96mngv12gek5a.apps.googleusercontent.com');
 
 
 app.listen(process.env.PORT || 8081)
